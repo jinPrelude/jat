c9070ea4175cc843a590b76adf4e059873eff89c
Euijin <jin.Prelude@gmail.com>
2025-03-03 19:17:59 +0900

    Fix to local path & Add jat-mini

--- scripts/generate_config_jat.py
+++ scripts/generate_config_jat.py
@@ -3,6 +3,26 @@ from transformers import AutoTokenizer, CLIPImageProcessor
 from jat.configuration_jat import JatConfig
 from jat.processing_jat import JatProcessor
 
+# Mini model
+tokenizer = AutoTokenizer.from_pretrained("gpt2", model_input_names=["input_ids", "attention_mask"])
+config = JatConfig(
+    vocab_size=tokenizer.vocab_size,
+    max_position_embeddings=512,
+    hidden_size=384,
+    num_layers=4,
+    attention_types=[[["global", "local"], 2]],
+    num_heads=6,
+    max_discrete_value=148 + 64,  # 148 (discrete obs from BabyAI) + 64 (max size of BabyAI's text observation)
+    tokenizer_class=tokenizer.__class__.__name__,
+)
+image_processor = CLIPImageProcessor(
+    size={"shortest_edge": config.image_size}, crop_size={"height": config.image_size, "width": config.image_size}
+)
+tokenizer.model_max_length = config.max_position_embeddings
+tokenizer.pad_token = tokenizer.eos_token
+processor = JatProcessor(tokenizer=tokenizer, image_processor=image_processor)
+config.save_pretrained("/scratch/euijinrnd/jat/local/jat-mini-config")
+processor.save_pretrained("/scratch/euijinrnd/jat/local/jat-mini")
 
 # Small model
 tokenizer = AutoTokenizer.from_pretrained("gpt2", model_input_names=["input_ids", "attention_mask"])
@@ -22,8 +42,8 @@ image_processor = CLIPImageProcessor(
 tokenizer.model_max_length = config.max_position_embeddings
 tokenizer.pad_token = tokenizer.eos_token
 processor = JatProcessor(tokenizer=tokenizer, image_processor=image_processor)
-config.push_to_hub("jat-project/jat-small")
-processor.push_to_hub("jat-project/jat-small")
+config.save_pretrained("/scratch/euijinrnd/jat/local/jat-small-config")
+processor.save_pretrained("/scratch/euijinrnd/jat/local/jat-small")
 
 # Medium model
 tokenizer = AutoTokenizer.from_pretrained("gpt2", model_input_names=["input_ids", "attention_mask"])
@@ -43,8 +63,8 @@ image_processor = CLIPImageProcessor(
 tokenizer.model_max_length = config.max_position_embeddings
 tokenizer.pad_token = tokenizer.eos_token
 processor = JatProcessor(tokenizer=tokenizer, image_processor=image_processor)
-config.push_to_hub("jat-project/jat-medium")
-processor.push_to_hub("jat-project/jat-medium")
+config.save_pretrained("/scratch/euijinrnd/jat/local/jat-medium-config")
+processor.save_pretrained("/scratch/euijinrnd/jat/local/jat-medium")
 
 # Large model
 tokenizer = AutoTokenizer.from_pretrained("gpt2", model_input_names=["input_ids", "attention_mask"])
@@ -64,5 +84,6 @@ image_processor = CLIPImageProcessor(
 tokenizer.model_max_length = config.max_position_embeddings
 tokenizer.pad_token = tokenizer.eos_token
 processor = JatProcessor(tokenizer=tokenizer, image_processor=image_processor)
-config.push_to_hub("jat-project/jat-large")
-processor.push_to_hub("jat-project/jat-large")
+config.save_pretrained("/scratch/euijinrnd/jat/local/jat-large-config")
+processor.save_pretrained("/scratch/euijinrnd/jat/local/jat-large")
+
